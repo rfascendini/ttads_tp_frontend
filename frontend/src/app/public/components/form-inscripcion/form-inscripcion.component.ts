00084a64435badc865ce13dc84797bd042b633d3
rfascendini <renzo.jrr10@gmail.com>
2023-08-05 16:46:03 -0300

    ACTUALIZACIONES

--- frontend/src/app/public/components/form-inscripcion/form-inscripcion.component.ts
+++ frontend/src/app/public/components/form-inscripcion/form-inscripcion.component.ts
@@ -7,12 +7,33 @@ import { Component } from '@angular/core';
 
 export class FormInscripcionComponent {
 
-  isChecked: boolean = false;
-  seccion_discapacidad: any;
+  // -------------------- SECCION DISCAPACIDAD
+  seccion_discapacidad: string = "";
 
+  // SUBSECCIONES DISCAPACIDAD
+  seccion_discapacidadNEAEAUDITI: boolean = false;
+  seccion_discapacidadNEAEVISUAL: boolean = false;
+  seccion_discapacidadNEAEMOTRIZ: boolean = false;
+  seccion_discapacidadNEAEPSICOS: boolean = false;
+  seccion_discapacidadNEAEOTRAS: boolean = false;
 
+  // -------------------- SECCION TRABAJO
+  seccion_trabajo: string = "";
 
-  
+  // -------------------- SECCION SECUNDARIO
+  secundario_completo: any = null;
+
+  // -------------------- SECCION UNIVERSITARIO
+  titulo_universitario_previo: string = "";
+  titulo_universitario_tramite: string = "";
+
+  // -------------------- SECCION DISCAPACIDAD
+  acceso_internet: string = "";
+  acceso_pc: string = "";
+
+  // -------------------- SECCION FAMILIA
+  padre_vive: string = "";
+  madre_vive: string = "";
 
 
 
