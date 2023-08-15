5612d8f8e8a75a5ba9d2a672053b36530cbf4a14
rfascendini <renzo.jrr10@gmail.com>
2023-08-15 20:03:59 -0300

    modificaicon footer

--- frontend/src/app/public/components/form-inscripcion/form-inscripcion.component.ts
+++ frontend/src/app/public/components/form-inscripcion/form-inscripcion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-inscripcion',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
 })
 
 export class FormInscripcionComponent {
+
 
   // -------------------- SECCION DISCAPACIDAD
   seccion_discapacidad: string = "";
