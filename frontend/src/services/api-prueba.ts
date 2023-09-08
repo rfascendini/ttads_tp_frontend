fb4e22a060d0b65d1ad27a963e3d04f72e0425fe
rfascendini <renzo.jrr10@gmail.com>
2023-09-08 12:32:48 -0300

    prueba de api funcionando

---
+++ frontend/src/services/api-prueba.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable, OnInit } from '@angular/core'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiPrueba implements OnInit {
+
+
+
+
+
+  }
+
+
+
+} export default ApiPrueba
